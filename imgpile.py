307454cb3370e5e0d047dcfa432cb6811b60fe58
Dan Stowell <dstowell@gmail.com>
2023-08-05 16:34:47 -0500

    store in sqlite

--- imgpile.py
+++ imgpile.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import re
+import sqlite3
 import subprocess
 import sys
 
@@ -10,9 +11,10 @@ logger = logging.getLogger('imgpile')
 
 def crawl_exifdata(roots):
     linere = re.compile('^(?P<name>.+\S)\s+:\s+(?P<value>\S.+)$')
-    names = ('File Type', 'Image Width', 'Image Height', 'Date/Time Original', 'Create Date')
+    names = ('File Name', 'File Type', 'Image Width', 'Image Height', 'Date/Time Original', 'Create Date')
     for root in roots:
         for (dirpath, dirnames, filenames) in os.walk(root):
+            logger.info('starting directory %s', dirpath)
             for filename in filenames:
                 path = os.path.join(dirpath, filename)
                 process = subprocess.run(('exiftool', path), capture_output=True)
@@ -30,9 +32,26 @@ def crawl_exifdata(roots):
 
 
 def main():
+    conn = sqlite3.connect('exifdata.db')
+    c = conn.cursor()
+    # c.execute('CREATE TABLE exifdata(path PRIMARY KEY, returncode, filename, filetype, width, height, datetime, createdate)')
+    # conn.commit()
     for exifdata in crawl_exifdata(sys.argv[1:]):
-        logger.info(exifdata)
-
+        t = (
+                exifdata.get('path'),
+                exifdata.get('returncode'),
+                exifdata.get('File Name'),
+                exifdata.get('File Type'),
+                exifdata.get('Image Width'),
+                exifdata.get('Image Height'),
+                exifdata.get('Date/Time Original'),
+                exifdata.get('Create Date'),
+        )
+        try:
+            c.execute('INSERT INTO exifdata VALUES(?, ?, ?, ?, ?, ?, ?, ?)', t)
+            conn.commit()
+        except sqlite3.IntegrityError:
+            continue
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
